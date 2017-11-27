e5086879f175e2f893a0d1ad6383dc13829ebc83
Mario <mariopalmero@gmail.com>
2017-11-27 22:39:22 +0100

    Added Indestructible blocks
    - Created a basic material for all the PBR meshes
    - Created a cube for the indestructible block
    - Set up a BP for the indestructible block
    - Added functionality in the MapManager to spawn the blocks

--- Source/BombermanLike/Map/Private/MapManager.cpp
+++ Source/BombermanLike/Map/Private/MapManager.cpp
@@ -1,9 +1,19 @@
 // - Mario Palmero [2017], zlib/libpng licensed.
 
 #include "Map/Public/MapManager.h"
+#include "ConstructorHelpers.h"
 
 
-void UMapManager::InitializeGrid()
+AMapManager::AMapManager() : Super()
+{
+	static ConstructorHelpers::FObjectFinder<UBlueprint> indestructibleBlockObject(TEXT("Blueprint'/Game/Environment/Blueprints/BP_IndestructibleBlock.BP_IndestructibleBlock'"));
+	if (indestructibleBlockObject.Object != nullptr)
+		IndestructibleBlock = indestructibleBlockObject.Object->GeneratedClass;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("SceneComponent");
+}
+
+void AMapManager::InitializeGrid()
 {
 	// Check if the number of rows or columns are odds
 	if (Rows % 2 != 1)
@@ -24,14 +34,36 @@ void UMapManager::InitializeGrid()
 	}
 }
 
-void UMapManager::PlaceIndestructibleBlocks()
+void AMapManager::PlaceIndestructibleBlocks()
 {
 	for (int indexRows = 0; indexRows < Rows; ++indexRows)
 	{
 		for (int indexColumns = 0; indexColumns < Columns; ++indexColumns)
 		{
+			
 			if (indexColumns % 2 == 1 && indexRows % 2 == 1)
+			{
+				// First we create the blocks in the abstract layer
 				m_map[indexRows][indexColumns] = EMapCellType::IndestructibleBlock;
+
+				AActor* block = GetWorld()->SpawnActor<AActor>(IndestructibleBlock);
+				block->SetActorLocation(GetTilePosition(indexColumns, indexRows));
+			}
 		}
 	}
+}
+
+FVector AMapManager::GetTilePosition(int column, int row) const
+{
+	// Positioning the block depending on it's matrix position
+	FVector blockLocation((0.5f + float(column) - Columns * 0.5f) * TileSize, (0.5f + float(row) - Rows * 0.5f) * TileSize, 0.0f);
+	blockLocation = GetActorQuat() * blockLocation;
+	blockLocation += GetActorLocation();
+	return blockLocation;
+}
+
+void AMapManager::BeginPlay()
+{
+	InitializeGrid();
+	PlaceIndestructibleBlocks();
 }
--- Source/BombermanLike/Map/Public/MapManager.h
+++ Source/BombermanLike/Map/Public/MapManager.h
@@ -16,7 +16,8 @@ enum EMapCellType
 	IndestructibleBlock,
 	DestructibleBlock,
 	PowerUp,
-	Bomb
+	Bomb,
+	Fire
 };
 
 /*! \brief Class that handles creation, progress and destruction of the map during a match
@@ -25,30 +26,49 @@ enum EMapCellType
  * along with the progress of the map during the match.
  */
 UCLASS()
-class UMapManager : public UObject
+class AMapManager : public AActor
 {
 	GENERATED_BODY()
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Map)
-	int Columns;	//<! Indicates the current number of columns. The map will generate this number of columns when initializing.
+	float TileSize;		//<! Sets the measure of the side of the tiles that compose the map
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Map)
-	int Rows;		//<! Indicates the current number of rows. The map will generate this number of rows when initializing.
+	int Columns;		//<! Indicates the current number of columns. The map will generate this number of columns when initializing.
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Map)
+	int Rows;			//<! Indicates the current number of rows. The map will generate this number of rows when initializing.
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = MapCreation)
+	UClass* IndestructibleBlock;	//<! Class for the indestructible blocks to spawn
 
 	/*! \brief Initializes the grid
 	*
 	* Method that fills all the cells with empty floor
 	*/
+	UFUNCTION()
 	void InitializeGrid();
-
 
 	/*! \brief Creates the non-destructible grid
 	 *
 	 * Method that fills the map with a grid of indestructible blocks
 	 */
+	UFUNCTION()
 	void PlaceIndestructibleBlocks();
 
+	/*! \brief Returns the position in world of a tile object
+	 *
+	 * Function that calculates the position of a tile depending on its coordinates in the map,
+	 * the position of this actor and its rotation
+	 */
+	UFUNCTION(BlueprintCallable, Category = Map)
+	FVector GetTilePosition(int column, int row) const;
+
+	AMapManager();		//<! Constructor
+
+protected:
+	virtual void BeginPlay() override;				//<! Called when the game starts or when spawned
 private:
-	std::vector<std::vector<EMapCellType>> m_map;
+	std::vector<std::vector<EMapCellType>> m_map;		//<! The abstraction of the physical map
 };
